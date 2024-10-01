cf3bb395019cd51fb2520b46a07336c544a0ab07
Suraj2813 <surajshah2813@gmail.com>
2024-10-01 12:56:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('order-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your order! We will contact you soon.');
+});
